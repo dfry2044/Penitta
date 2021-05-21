59085f78c7b2f16334df716a55f4a9b4cf9351fa
maro1220 <70903213+maro1220@users.noreply.github.com>
2021-05-21 13:31:11 +0900

    main 작업중 임시 저장

---
+++ js/index.js
@@ -0,0 +1,55 @@
+
+//c1 - slider
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+//c2 - slider 
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".mySwiper2 .swiper-pagination",
+        clickable: true,
+    },
+});
+
+//c-3 slider
+var popImg = document.querySelector('.pop-img-img');
+var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 'auto',
+    loop: true,
+    centeredSlides: true,
+    grabCursor: true,
+    on: {
+        transitionStart:function(){
+            popImg.classList.remove('active');
+        },
+        transitionEnd:function(){
+            var activeSlider = document.querySelector('.mySwiper3 .swiper-slide-active img').getAttribute( 'src' );
+            popImg.classList.add('active');
+            popImg.setAttribute('src',activeSlider);
+        },
+    }
+});
+
+//header-scroll 
+ScrollTrigger.create({
+    start: 'top -80',
+    end: 99999,
+    toggleClass: {className: 'active', targets: 'header'}
+});
+
+
+//pop-img-up
+var pop = document.querySelector('.pop-img');
+gsap.fromTo(pop, {},{
+        scrollTrigger: {
+        trigger: '.c3',
+        start: 'top-=300',
+        end:'+=1500',
+        toggleClass: {className: 'active', targets: '.pop-img'}
+     }
+});
