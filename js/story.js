a28a99aa4d6e822af678745adbdeb60539af1268
천지연 <cheonz06@naver.com>
2021-06-02 13:33:13 +0900

    story header 모션 수정

--- js/story.js
+++ js/story.js
@@ -1,3 +1,54 @@
+//header
+ $(window).scroll(function () {
+     var browserT = $(window).scrollTop();
+     console.log(browserT);
+
+     $('header').addClass('on');
+     if (browserT == 0) {
+         $('header').removeClass('on');
+     }
+ });
+
+ $(window).scroll(function () {
+     var browserT = $(window).scrollTop();
+     console.log(browserT);
+
+     $('header').addClass('active');
+     if (browserT == 0) {
+         $('header').removeClass('active');
+     }
+ });
+
+ $(window).scroll(function () {
+        var browserT = $(window).scrollTop();
+        console.log(browserT);
+
+        $('.header-left').addClass('active');
+        if (browserT == 0) {
+            $('.header-left').removeClass('active');
+        }
+    });
+
+ $(window).scroll(function () {
+        var browserT = $(window).scrollTop();
+        console.log(browserT);
+
+        $('header div.header-right > div.userMenu').addClass('active');
+        if (browserT == 0) {
+            $('header div.header-right > div.userMenu').removeClass('active');
+        }
+    });
+
+ /*$(window).scroll(function () {
+     var browserT = $(window).scrollTop();
+     console.log(browserT);
+
+     $('.wrap').addClass('active');
+     if (browserT == 0) {
+         $('.wrap').removeClass('active');
+     }
+ });*/
+
 //c2 swiper
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
@@ -16,14 +67,16 @@ var swiper = new Swiper(".mySwiper", {
 
 //c4 button
 
-$(function(){
-			$('.list_item').hide();
-			$('.list_item').slice(0, 8).show();
-			$('.button').click(function(e){
-				e.preventDefault();
-				$('.list_item:hidden').slice(0, 4).show();
-				if($('.list_item:hidden').length == 0) {
-					alert("더이상 항목이 없습니다.")
-				}
-			})
-		})
+$(function () {
+    $('.list_item').hide();
+    $('.list_item').slice(0, 8).show();
+    $('.button').click(function (e) {
+        e.preventDefault();
+        $('.list_item:hidden').slice(0, 4).show();
+        if ($('.list_item:hidden').length == 0) {
+            alert("더이상 항목이 없습니다.")
+        }
+    })
+})
+
+
