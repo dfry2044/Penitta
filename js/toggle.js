0b7044485457221b2e45508c221b3e93a65e965f
이승열 <marudev4905@gmail.com>
2021-08-01 06:17:33 +0900

    나의 스토리 툴팁 이슈 수정

--- js/toggle.js
+++ js/toggle.js
@@ -13,7 +13,10 @@ var etcTooltipBtn = $(".card-etc");
 var etcTooltip = $(".card-etc-tooltip");
 
 $(document).on('click',function(e){
-    if(e.target.parentElement.classList.value == `card-etc`){
+    console.log(
+        e.target.parentElement.classList.value 
+    )
+    if(e.target.parentElement.classList.value == `card-etc` || e.target.parentElement.classList.value == "card-etc ico-trash"){
         e.target.parentElement.nextElementSibling.classList.add("show")
     }else{
         etcTooltip.removeClass("show");
