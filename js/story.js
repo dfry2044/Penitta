57ea2bfa793096869a3906b763395ba5018c88e2
cheonjiyeon <cheonz06@naver.com>
2021-08-02 12:07:04 +0900

    팝업 반응형 및 팝업 공통화 작업 진행중

--- js/story.js
+++ js/story.js
@@ -44,12 +44,18 @@ var swiper = new Swiper(".mySwiper2", {
         clickable: true,
     },
     breakpoints: {
+        1920: {
+            slidesPerView: '3',
+            slidesPerGroup: 3,
+            freeMode: false,
+            spaceBetween: 27,
+        },
         1024: {
             slidesPerView: 'auto',
             freeMode: true,
             spaceBetween: 20,
-        }
-      }
+        },
+      },
 });
 
 //c4 button
@@ -105,63 +111,3 @@ storyico.click(function(){
     
 //     return false;
 // })
-
-
-$('.card-etc-tooltip > button:nth-child(3)').click(function(){
-    $('.share-popup').addClass('active');
-    scrollDisable();
-});
-
-$('.share-ico-close').click(function(){
-    $('.share-popup').removeClass('active');
-    scrollAble();
-});
-
-$('.card-etc-tooltip > button:nth-child(2)').click(function(){
-    $('.tag-popup').addClass('active');
-    scrollDisable();
-});
-
-$('.tag-ico-close').click(function(){
-    $('.tag-popup').removeClass('active');
-    scrollAble();
-});
-
-$('.list-top > a').click(function(){
-    $('.view-popup').addClass('active');
-
-    scrollDisable();
-});
-
-$('.view-ico-close').click(function(){
-    $('.view-popup').removeClass('active');
-
-    scrollAble();
-});
-
-$('.c4-react > button:nth-child(3)').click(function(){
-    $('.comment-popup').addClass('active');
-    scrollDisable();
-});
-
-$('.comment-ico-close').click(function(){
-    $('.comment-popup').removeClass('active');
-    scrollAble();
-});
-
-$(".filter-open-btn").click(function(){
-    $(".filter-popup").addClass("show");
-    scrollDisable();
-})
-
-$(".filter-popup-close-btn").click(function(){
-    $(".filter-popup").removeClass("show");
-    scrollAble();
-})
-
-function scrollDisable(){
-    $('body').addClass('scrollDisable');
-}
-function scrollAble(){
-    $('body').removeClass('scrollDisable');
-}
